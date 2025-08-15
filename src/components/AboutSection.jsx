d975c4d1ec293b1e7bcae8c8c9e84123e239c974
Akki-luffy <akhilkompally27@gmail.com>
2025-08-15 18:35:24 +0530

    Cursor added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -39,7 +39,7 @@ export const AboutSection = () => {
                 href={Resume}
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
-                Download CV
+                See My Resume
               </a>
             </div>
           </div>
